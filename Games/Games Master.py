5751aff3d161a94bb6ae0128412c44a62005e5a8
G Kirilov <kirilov77@protonmail.com>
2023-01-15 12:21:52 +0200

    Update Games Master.py

--- Games/Games Master.py
+++ Games/Games Master.py
@@ -4,38 +4,35 @@ class Gamemaster:
         print(""
               "Welcome. The game options are:\n"
               "1. Roulette\n"
-              "2. Blackjack\n"
-              "3. Hangman\n"
-              "4. Slots\n"
-              "5. Craps\n"
-              "6. Exit\n")
+              "2. Hangman\n"
+              "3. Slots\n"
+              "4. Craps\n"
+              "5. Exit\n")
 
-    choices = {1: "Roulette", 2: "Blackjack", 3: "Hangman", 4: "Slots", 5: "Craps", 6: "Exit"}
+    choices = {1: "Roulette", 2: "Hangman", 3: "Slots", 4: "Craps", 5: "Exit"}
 
     # Choosing a game and error proofing
     def game_choice(self):
         while True:
             try:
-                choice = int(input("Select the number of the game you would like to play or 6 to quit\n"))
+                choice = int(input("Select the number of the game you would like to play or 5 to quit\n"))
                 if choice not in self.choices.keys():
                     print("Unavailable option")
                     continue
             except ValueError:
-                print("Enter a number from 1 to 6 please. Integers only")
+                print("Enter a number from 1 to 5 please. Integers only")
                 continue
             else:
                 print(f"You have chosen {self.choices[choice]}")
                 if choice == 1:
                     import Roulette
                 if choice == 2:
-                    import BlackJack
-                if choice == 3:
                     import Hangman
-                if choice == 4:
+                if choice == 3:
                     import Slots
-                if choice == 5:
+                if choice == 4:
                     import Craps
-                if choice == 6:
+                if choice == 5:
                     quit()
                     break
 
