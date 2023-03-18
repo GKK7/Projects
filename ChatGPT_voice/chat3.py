fc5d0877f56814fb087230b3bdb91384a1d50869
G Kirilov <kirilov77@protonmail.com>
2023-03-18 17:54:25 +0200

    Update chat3.py

--- ChatGPT_voice/chat3.py
+++ ChatGPT_voice/chat3.py
@@ -1,64 +1,46 @@
 import openai
 import speech_recognition as sr
-import platform
-import logging
-import sys
+import pyttsx3
+
 
 # Initialize OpenAI API
 openai.api_key = "YOUR_API_KEY_HERE"
-
 # Initialize the text to speech engine
-if platform.system() == "Windows":
-    import win32com.client
-    engine = win32com.client.Dispatch("SAPI.SpVoice")
-else:
-    import os
-    import pyttsx3
-    engine = pyttsx3.init()
-
-# Set up logging
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+engine = pyttsx3.init()
+engine.setProperty('voice', 'en-US')
 
 
-def transcribe_audio_to_test(filename):
+def transcribe_audio_to_text(filename):
     recognizer = sr.Recognizer()
     with sr.AudioFile(filename) as source:
         audio = recognizer.record(source)
     try:
         return recognizer.recognize_google(audio)
-    except sr.RequestError as e:
-        logging.error("Could not transcribe audio: {}".format(e))
-    except sr.UnknownValueError as e:
-        logging.error("Could not transcribe audio: {}".format(e))
+    except:
+        print("Unknown error occurred")
 
 
 def generate_response(prompt):
-    try:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=4000,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-        return response["choices"][0]["text"]
-    except openai.Error as e:
-        logging.error("Could not generate response: {}".format(e))
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=4000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response["choices"][0]["text"]
 
 
 def speak_text(text):
-    try:
-        engine.say(text)
-        engine.runAndWait()
-    except Exception as e:
-        logging.error("Could not speak text: {}".format(e))
+    engine.say(text)
+    engine.runAndWait()
 
 
 def main():
     while True:
         # Wait for user to say "yo"
-        logging.info("Say 'Yo' to start recording your question")
+        print("Say 'Yo' to start recording your question or 'Goodbye' to exit the program.")
         with sr.Microphone() as source:
             recognizer = sr.Recognizer()
             audio = recognizer.listen(source)
@@ -67,7 +49,7 @@ def main():
                 if transcription.lower() == "yo":
                     # Record audio
                     filename = "input.wav"
-                    logging.info("Say your question")
+                    print("Say your question")
                     with sr.Microphone() as source:
                         recognizer = sr.Recognizer()
                         source.pause_threshold = 1
@@ -75,22 +57,23 @@ def main():
                         with open(filename, "wb") as f:
                             f.write(audio.get_wav_data())
 
-                    # Transcribe audio to text
-                    text = transcribe_audio_to_test(filename)
+                    # Transcript audio to text
+                    text = transcribe_audio_to_text(filename)
                     if text:
-                        logging.info("You said: {}".format(text))
+                        print(f"You said: {text}")
 
-                        # Generate response
+                        # Generate the response
                         response = generate_response(text)
-                        if response:
-                            logging.info("Chat GPT-3 says: {}".format(response))
+                        print(f"Chat GPT-3 says: {response}")
 
-                            # Speak response
-                            speak_text(response)
-            except sr.RequestError as e:
-                logging.error("Could not recognize speech: {}".format(e))
-            except sr.UnknownValueError as e:
-                logging.error("Could not recognize speech: {}".format(e))
+                        # Read response using GPT-3
+                        speak_text(response)
+                elif transcription.lower() == "goodbye":
+                    # Terminate the program
+                    print("Exiting the program. Goodbye!")
+                    return
+            except Exception as e:
+                print("An error occurred: {}".format(e))
 
 
 if __name__ == "__main__":
